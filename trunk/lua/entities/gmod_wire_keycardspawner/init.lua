4d1d123f0255451ce5dc6992d1904f97ed3dee49
anticept <anticept@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-06-04 18:50:28 +0000

    --- yaml --- svn_rev: 10 current_ref: refs/heads/master current_commit: 68c57ca55d33461973b1eb9ac0d0ef4679148cbe head_branch: refs/heads/master migrated_from: v3

---
+++ trunk/lua/entities/gmod_wire_keycardspawner/init.lua
@@ -0,0 +1,53 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include('shared.lua')
+
+ENT.WireDebugName = "KeycardSpawner"
+
+util.PrecacheSound('buttons/button9.wav')
+
+util.PrecacheSound('buttons/button11.wav')
+
+local MODEL = Model("models/keycardspawner/keycardspawner.mdl")
+function ENT:Initialize()
+	self.Entity:SetModel( MODEL )
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )
+	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
+	self.Entity:SetSolid( SOLID_VPHYSICS )
+	self.Inputs = Wire_CreateInputs(self.Entity, { "Spawn" })
+	self:ShowOutput()
+end
+
+function ENT:SetLockCode(value)
+	self.LockCode = (value or 0)
+end
+
+function ENT:TriggerInput(iname, value)
+	if (iname == "Spawn" and value == 1) then
+                local keycard = MakeWireKeycard(self.Entity:GetOwner(), self.Entity:GetAngles(), self.Entity:GetPos() + (self.Entity:GetUp() * 4), self.LockCode)
+		if (keycard) then
+			self.Entity:EmitSound('buttons/button9.wav')
+        	else
+			self.Entity:EmitSound('buttons/button11.wav')
+		end
+	end
+end
+
+
+function MakeWireKeycard( pl, ang, Pos, lockcode )
+	local wire_keycard = ents.Create( "gmod_wire_keycard" )
+	// ang.Pitch = ang.Pitch + 90
+	if (!wire_keycard:IsValid()) then return false end
+	wire_keycard:SetPos( Pos )
+	wire_keycard:SetAngles( ang )
+	wire_keycard:Setup(pl)
+	wire_keycard:SetLockCode(lockcode)
+        wire_keycard:ResetValues()
+	wire_keycard:Spawn()
+	return wire_keycard
+end
+
+function ENT:ShowOutput()
+	self:SetOverlayText("Wire Keycard Spawner\nLock Code: "..self.LockCode)
+end
---
+++ trunk/lua/entities/gmod_wire_keycardspawner/shared.lua
@@ -0,0 +1,6 @@
+ENT.Type 		= "anim"
+ENT.Base 		= "base_wire_entity"
+
+ENT.PrintName	= "Keycard Spawner (Wire)"
+ENT.Author		= "Jesse Dubay (adb)"
+ENT.Contact		= "jesse@thefortytwo.net"
