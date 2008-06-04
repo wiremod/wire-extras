70b7111a0c1514f8d3cd8978a1c1f74a66b1a7fc
anticept <anticept@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-06-04 03:48:49 +0000

    Added Ballsocket_adv_ez, Facer, Freezer, multi_wire, and servo.
    ---
    yaml
    ---
    svn_rev: 3
    current_ref: refs/heads/master
    current_commit: 8057986ef417ac0bb844f71123807f6df86b75a1
    head_branch: refs/heads/master
    migrated_from: 3

---
+++ trunk/lua/entities/gmod_wire_servo4/shared.lua
@@ -0,0 +1,14 @@
+
+
+ENT.Type 			= "anim"
+ENT.Base 			= "base_wire_entity"
+
+ENT.PrintName		= "WireServo"
+ENT.Author			= "Exitlights"
+ENT.Contact			= ""
+ENT.Purpose			= "Creates a servo that rotates around its yaw, pitch, or roll axis."
+ENT.Instructions	= ""
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
+
