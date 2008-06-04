fb99d8ef99d32e0b8c754aaea047ee17f2714342
anticept <anticept@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-06-04 03:48:49 +0000

    Added Ballsocket_adv_ez, Facer, Freezer, multi_wire, and servo.
    ---
    yaml
    ---
    svn_rev: 3
    current_ref: refs/heads/master
    current_commit: 8057986ef417ac0bb844f71123807f6df86b75a1
    head_branch: refs/heads/master
    migrated_from: v3

---
+++ trunk/lua/weapons/gmod_tool/stools/wire_freezer.lua
@@ -0,0 +1,90 @@
+
+TOOL.Category		= "Wire - Physics"
+TOOL.Name			= "Freezer"
+TOOL.Command		= nil
+TOOL.ConfigName		= ""
+
+if CLIENT then
+	
+    language.Add( "Tool_wire_freezer_name", "Freezer Tool (Wire)" )
+    language.Add( "Tool_wire_freezer_desc", "Makes a controllable freezer" )
+    language.Add( "Tool_wire_freezer_0", "Primary: Click on the entity to be frozen" )
+    language.Add( "Tool_wire_freezer_1", "Left click to place the controller" )
+	language.Add( "undone_wirefreezer", "Undone Wire Freezer" )
+	
+end
+
+function TOOL:LeftClick( trace )
+	
+	if ( trace.Entity:IsValid() && trace.Entity:IsPlayer() ) then return end
+	if ( SERVER && !util.IsValidPhysicsObject( trace.Entity, trace.PhysicsBone ) ) then return false end
+	local iNum = self:NumObjects()
+		self:SetObject( iNum + 1, trace.Entity, trace.HitPos, Phys, trace.PhysicsBone, trace.HitNormal )
+	
+	if ( iNum == 1 ) then
+			
+		if ( CLIENT ) then
+			self:ClearObjects()
+			return true
+		end
+		
+		local ply = self:GetOwner()
+		local Ent1 = self:GetEnt(1)
+				
+		// Attach our Controller to the weld constraint
+		local Ang = trace.HitNormal:Angle()
+		Ang.pitch = Ang.pitch + 90
+		local controller = MakeWireFreezerController( ply, trace.HitPos, Ang )
+		
+		// Send Entity and Constraint info over to the controller
+		controller:SendVars(Ent1)
+		
+		local min = controller:OBBMins()
+		controller:SetPos( trace.HitPos - trace.HitNormal * min.z )
+				
+		undo.Create("WireFreezer")
+			undo.AddEntity( controller )
+			undo.SetPlayer( ply )
+		undo.Finish()
+
+		elseif ( iNum == 1 ) then
+		
+		if ( CLIENT ) then
+			return true
+		end
+		
+		// Get information we're about to use
+		self.Ent1 = self:GetEnt(1)
+							
+		self:SetStage(0)
+		
+	else
+		
+		self:SetStage( iNum + 1 )
+		
+	end
+	
+	return true
+	
+end
+
+if SERVER then
+	
+	function MakeWireFreezerController( pl, Pos, Ang )
+		local controller = ents.Create("gmod_wire_freezer")
+		
+		controller:SetPos( Pos )
+		controller:SetAngles( Ang )
+		controller:SetPlayer(pl)
+		
+		controller:Spawn()
+		
+		return controller
+	end
+	duplicator.RegisterEntityClass("gmod_wire_freezer", MakeWireFreezerController, "Pos", "Ang")
+		
+end
+
+function TOOL.BuildCPanel( panel )
+	panel:AddControl( "Header", { Text = "#Tool_wire_freezer_name", Description = "#Tool_wire_freezer_desc" } )
+end
