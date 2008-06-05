fece194c6859f460787eea53792b387796d3ff35
anticept <anticept@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-06-05 04:00:42 +0000

    --- yaml --- svn_rev: 12 current_ref: refs/heads/master current_commit: 7b90bab9be334c39290f2bf3273c31de259fa0dd head_branch: refs/heads/master migrated_from: v3

---
+++ trunk/lua/entities/gmod_wire_pid/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_wire_entity"
+
+ENT.PrintName		= "PID Controller"
+ENT.Author			= "Moyer"
+ENT.Contact			= "Nope"
+ENT.Purpose			= "Has no purpose"
+ENT.Instructions	= ""
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
