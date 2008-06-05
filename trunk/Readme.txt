b0cbdcdd4006730425a55880c7828867ab4e4a92
anticept <anticept@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-06-05 04:00:42 +0000

    Added Insanely huge memory and phi gate, PID (a super delta), xyz beacon, and wire keypad.
    ---
    yaml
    ---
    svn_rev: 12
    current_ref: refs/heads/master
    current_commit: 7b90bab9be334c39290f2bf3273c31de259fa0dd
    head_branch: refs/heads/master
    migrated_from: 3

--- trunk/Readme.txt
+++ trunk/Readme.txt
@@ -11,23 +11,27 @@ Addon Contributed - developer - link - license (if not GPL v3):
 
 Buoyancy Stool - RabidToaster - http://forums.facepunchstudios.com/showthread.php?p=10186318
 Door Stool - Doridian - http://forums.facepunchstudios.com/showthread.php?t=480569
-Facer - Incompatible - http://www.garrysmod.org/downloads/?a=view&id=20074
+Facer - Incompatible - http://www.wiremod.com/f16/wire-freezer-2352/
 Freezer - Incompatible - http://www.wiremod.com/2352-wire-freezer.html
-Free Fall's Extentions - (temp link) http://www.wiremod.com/f16/free-fall-extension-wire-socket-radio-other-stuff-i-made-5129/
+Free Fall's Extentions - http://www.wiremod.com/f16/free-fall-extension-wire-socket-radio-other-stuff-i-made-5129/
 	Adv. Dupe. Teleporter
 	String Gate (for teleport)
-_Kilburn's Addons - http://www.wiremod.com/4294-addons-wire-mod-mods-wire-addon.html:
+Insanely Huge Memory and Phi Gate - MagnetoHydroDynamics - http://www.wiremod.com/f16/insanely-huge-memory-chips-phi-gate-5018/
+_Kilburn's Addons - http://www.wiremod.com/f16/addons-wire-mod-mods-wire-addon-4294/:
 	Wire String Gates
 	Wire Materializer
 	Wire Painter
 	Wire Motor
 	Wire Microphone
-Keycards - adb - (temp link) http://www.wiremod.com/f16/wire-keycards-2143/
-Lever - cpf - (temp link) http://www.wiremod.com/f16/wire-lever-4097/
-Multi-wire tool - Incompatible (apparently abandoned) - http://www.wiremod.com/2545-multi-wire-tool.html
-No Collide Addon - splat2010 - http://www.wiremod.com/4777-no-collide-addon.html
-Notifier - high6 - (temp link) http://www.wiremod.com/f16/notifier-3204/
+Keycards - adb - http://www.wiremod.com/f16/wire-keycards-2143/
+Keypad - Robbis_1 - http://forums.facepunchstudios.com/showthread.php?t=276931
+Lever - cpf - http://www.wiremod.com/f16/wire-lever-4097/
+Multi-wire tool - Incompatible (apparently abandoned) - http://www.wiremod.com/f16/multi-wire-tool-2545/
+No Collide Addon - splat2010 - http://www.wiremod.com/f16/no-collide-addon-4777/
+Notifier - high6 - http://www.wiremod.com/f16/notifier-3204/
+PID Controller (super delta) - Moyer - http://www.wiremod.com/f16/pid-controller-5061/
 Servo - Unvarifiable Developer (Incompatible or Exlights are most likely) - No Confirmed Home Thread
+XYZ Beacon - Doticatto - http://www.wiremod.com/f16/wired-xyz-beacon-eject-input-socket-4708/
 
 
 Copyright CoreyLee Hassell (Anticept), and respective developers, 2007, 2008.
---
+++ trunk/lua/entities/gmod_wire_pid/init.lua
@@ -0,0 +1,108 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include('shared.lua')
+
+ENT.WireDebugName = "PID"
+
+function ENT:Initialize()
+	/* Make Physics work */
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )
+	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
+	self.Entity:SetSolid( SOLID_VPHYSICS )
+
+	/* Set wire I/O */
+	self.Inputs = Wire_CreateInputs(self.Entity, { "In", "Set Point", "Enable" })
+	self.Outputs = Wire_CreateOutputs(self.Entity, { "Out" })
+
+	/* Initialize values */
+	self.set = 0
+	self.inval = 0
+	self.p = 0
+	self.i = 0
+	self.d = 0
+	self.dcut = 0
+	self.ilim = 0
+	self.iterm = 0
+	self.enabled = 1
+
+	local phys = self.Entity:GetPhysicsObject()
+	if (phys:IsValid() == true) then
+		phys:Wake()
+	end
+end
+
+function ENT:Think()
+	/* Make sure the gate updates even if we don't receive any input */
+	self:TriggerInput()
+end
+
+function ENT:SetupGains(p, i, d, dcut, ilim)
+	/* Called by creator to set options */
+	self.p = p
+	self.i = i
+	self.d = d
+	self.dcut = dcut
+	self.ilim = ilim
+	self.lasttime = CurTime()
+	self.lasterror = 0
+	self.iterm = 0
+end
+
+function ENT:TriggerInput(iname, value)
+	/* Change variables to reflect input */
+	if (iname == "Set Point") then
+		self.set = value
+	end
+	if (iname == "In") then
+		self.inval = value
+	end
+
+	if (iname == "Enable") then
+		self.enabled = value
+	end
+
+	/* If we're not enabled, set the output to zero and exit */
+	if (self.enabled == 0) then
+		Wire_TriggerOutput(self.Entity, "Out", 0)
+		return
+	end
+
+	/* Define some local variables */
+	local error = self.set - self.inval
+	local dt = CurTime() - self.lasttime
+
+	/* Calculate derivative term (de/dt) , check for divide by zero */
+	local dterm = 0
+	if (dt>0) then
+		dterm = (error - self.lasterror)/dt
+	else
+		dterm = 0
+	end
+	dterm = dterm * self.d
+
+	/* If the derivative term is less than the cutoff, evaluate the integral term */
+	if (math.abs(dterm) < self.dcut) then
+		self.iterm = self.iterm + self.i * error
+	end
+
+	/* Bound the integral term to the user limit */
+	if (self.iterm > self.ilim && error > 0) then
+		self.iterm = self.ilim
+	end
+	if (self.iterm < -self.ilim && error < 0) then
+		self.iterm = -self.ilim
+	end
+
+	/* Setup for next time */
+	self.lasttime = CurTime()
+	self.lasterror = error
+
+	/* Output it */
+	self.out = (self.p * error) + self.iterm + dterm
+	Wire_TriggerOutput(self.Entity, "Out", self.out)
+end
+
+
+
