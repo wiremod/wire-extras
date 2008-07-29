1c2ce9fc34aa92123258262a26138f90ab25137a
anticept <anticept@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-07-29 03:01:38 +0000

    --- yaml --- r: 25 b: refs/heads/master c: d44a1d10863d5a6d41edde1415176c07df345eb0 h: refs/heads/master i:   23: 929511f95663a2941169b23aca81cb7be692a0e7 v: v3

---
+++ trunk/lua/autorun/server/player_tfextension.lua
@@ -0,0 +1,52 @@
+//-----------------------------------------------------------------------------
+// Player Extension - Target Lock for Wire Target Finder
+//-----------------------------------------------------------------------------
+
+local meta = FindMetaTable ("Player")
+if (!meta) then
+    Msg ("Failed to load Wire Player Extension for Target Finder!")
+    return
+end
+
+// Variable to track lock count
+meta.m_TargetLocks = {}
+
+
+//-----------------------------------------------------------------------------
+// Name: Player:TargetFinderLock()
+// Desc: Called by target finder when it locks onto this player.
+//-----------------------------------------------------------------------------
+
+function meta:TargetFinderLock (ent)
+    if (!table.HasValue (self.m_TargetLocks, ent)) then
+        table.insert (self.m_TargetLocks, ent)
+    end
+end
+
+
+//-----------------------------------------------------------------------------
+// Name: Player:TargetFinderUnlock()
+// Desc: Called by target finder when it unlocks this player
+//-----------------------------------------------------------------------------
+
+function meta:TargetFinderUnlock (ent)
+    if (table.HasValue (self.m_TargetLocks, ent)) then
+        for k,v in ipairs (self.m_TargetLocks) do
+            if (v == ent) then
+                table.remove (self.m_TargetLocks, k)
+                return
+            end
+        end
+    end
+end
+
+
+//-----------------------------------------------------------------------------
+// Name: Player:IsLockedOnto ()
+// Desc: Returns true if this player is a target, false otherwise.
+//-----------------------------------------------------------------------------
+
+function meta:IsLockedOnto ()
+    return (table.getn (self.m_TargetLocks) > 0)
+end
+
