2572166159ad3a79a861005e53b26c0e9269089b
Hitman271 <Hitman271@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-08-03 01:36:35 +0000

    Updated Wire Npcs

--- lua/entities/npc_controller/init.lua
+++ lua/entities/npc_controller/init.lua
@@ -1,57 +1,49 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
-include( 'shared.lua' )
 
-local ison = 0
+include('shared.lua')
 
-local model = "models/jaanus/wiretool/wiretool_siren.mdl"
+local X = 0
+local Y = 0
+local Z = 0
+local Go = 0
 
-function ENT:Initialize() 
-	self.Entity:SetModel( model ) 	
-	self.Entity:PhysicsInit( SOLID_VPHYSICS )     
-	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )   
-	self.Entity:SetSolid( SOLID_VPHYSICS )               
-	local phys = self.Entity:GetPhysicsObject()  
-		if not (WireAddon == nil) then self.Inputs = Wire_CreateInputs(self.Entity, { "X", "Y", "GO" }) end	
-	    	if not (WireAddon == nil) then self.Outputs = Wire_CreateOutputs(self.Entity, { "On"}) end
-	if (phys:IsValid()) then  		
-		phys:Wake()  	
-	end 
- 
-end   
+function ENT:Initialize()
+	self:SetModel( "models/Humans/Group01/Female_01.mdl" )
+	self:SetHullType( HULL_HUMAN );
+	self:SetHullSizeNormal();	
+	self:SetSolid( SOLID_BBOX ) 
+	self:SetMoveType( MOVETYPE_STEP )	
+	self:CapabilitiesAdd( CAP_MOVE_GROUND | CAP_OPEN_DOORS | CAP_ANIMATEDFACE | CAP_TURN_HEAD | CAP_USE_SHOT_REGULATOR | CAP_AIM_GUN )	
+	self:SetMaxYawSpeed( 5000 )	
+		//don't touch stuff above here
 
-function ENT:TriggerInput( iname, value ) 
-	if ( iname == "X" ) then
-		local X = value
-	end
-	if ( iname == "Y" ) then
-		local Y = value
-	end
-	if ( iname == "GO" ) then
-		local GO = value
-		self.ison = value
-	end
-	SendVars( X, Y, GO )
+	self:SetHealth(30)
+	self:Give( "ai_weapon_smg1" ) //Can be given sweps.
 end
 
-function SendVars( X, Y, GO )// This has to be routed somehow
-	Npc_C_271 = {} // Just a custom variable to be sure of no duplicates
-	local m = Npc_C_271
-	m.X = X
-	m.Y = Y
-	m.GO = GO
-	Npc_C_271 = m
+function ENT:OnTakeDamage(dmg)
+	local NewHealth = (self:Health() - dmg:GetDamage())
+  	self:SetHealth( NewHealth )
+  		if self:Health() <= 0 then //run on death
+			self:SetNPCState( NPC_STATE_DEAD )
+  			self:SetSchedule( SCHED_FALL_TO_GROUND )
+  	end
+end 
+
+
+function ENT:Think( Xt, Yt, Zt, Got )// recieve the vars from sendvars
+	self.X, self.Y, self.Z, self.Go = Xt, Yt, Zt, Got
+	self:SetLastPosition( Vector ( self.X, self.Y, self.Z ))
+	Msg("The X is "..self.X.." the Y is "..self.Y.." and the Z is"..self.Z.."/n")
+	if ( self.Go == 1 ) then
+		self:SetSchedule( SCHED_FORCED_GO_RUN )
+	elseif ( self.Go == 0 ) or ( Got == nil ) then
+		self:SetSchedule( SCHED_IDLE_STAND )
+	end
 end
 
-function ENT:Think()
-	if ( self.ison == 1 ) then
-		self:SetOverlayText( " Npc is moving " )
-	else
-		self:SetOverlayText( " Npc is idle " )
-	end
-	if not ( WireAddon == nil ) then Wire_TriggerOutput( self.Entity, "On", self.ison ) end
-end
-	
+
 
 
 
