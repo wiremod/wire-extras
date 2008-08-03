91ee86abfe5e658e9ceb9b42541a06c9dd90a234
Hitman271 <Hitman271@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-08-03 01:36:35 +0000

    --- yaml --- svn_rev: 27 current_ref: refs/heads/master current_commit: 2572166159ad3a79a861005e53b26c0e9269089b head_branch: refs/heads/master index:   25: 041dffa3b13ebdf545934358a2a33182f369ee1e   23: 86a3c055428884182e0f7e6b9a09f20abcc11ae3 migrated_from: v3

--- trunk/lua/entities/npc_npc/init.lua
+++ trunk/lua/entities/npc_npc/init.lua
@@ -3,22 +3,22 @@ AddCSLuaFile( "shared.lua" )
 
 include('shared.lua')
 
-function ENT:Initialize()
+local X = 0
+local Y = 0
+local Z = 0
+local Go = 0
 
+function ENT:Initialize()
 	self:SetModel( "models/Humans/Group01/Female_01.mdl" )
-	
 	self:SetHullType( HULL_HUMAN );
-	self:SetHullSizeNormal();
-	
+	self:SetHullSizeNormal();	
 	self:SetSolid( SOLID_BBOX ) 
-	self:SetMoveType( MOVETYPE_STEP )
-	
-	self:CapabilitiesAdd( CAP_MOVE_GROUND | CAP_OPEN_DOORS | CAP_ANIMATEDFACE | CAP_TURN_HEAD | CAP_USE_SHOT_REGULATOR | CAP_AIM_GUN )
-	
-	self:SetMaxYawSpeed( 5000 )
-	
-	//don't touch stuff above here
-	self:SetHealth(100)
+	self:SetMoveType( MOVETYPE_STEP )	
+	self:CapabilitiesAdd( CAP_MOVE_GROUND | CAP_OPEN_DOORS | CAP_ANIMATEDFACE | CAP_TURN_HEAD | CAP_USE_SHOT_REGULATOR | CAP_AIM_GUN )	
+	self:SetMaxYawSpeed( 5000 )	
+		//don't touch stuff above here
+
+	self:SetHealth(30)
 	self:Give( "ai_weapon_smg1" ) //Can be given sweps.
 end
 
@@ -27,23 +27,24 @@ function ENT:OnTakeDamage(dmg)
   	self:SetHealth( NewHealth )
   		if self:Health() <= 0 then //run on death
 			self:SetNPCState( NPC_STATE_DEAD )
-  			self:SetSchedule( SCHED_FALL_TO_GROUND ) //because it's given a new schedule, the old one will end.
+  			self:SetSchedule( SCHED_FALL_TO_GROUND )
   	end
 end 
 
-//function ENT:TaskFinished()
-//	//return true
-//end
 
-function ENT:Think()// Get Npc_C_271 then set the X, and Y to it
-	local m = Npc_C_271
-	local X = m.X
-	local Y = m.Y
-	local GO = m.GO
-	self:SetLastPosition( vector( X, Y, 0 ))
-	if ( GO == 1 ) then
+function ENT:Think( Xt, Yt, Zt, Got )// recieve the vars from sendvars
+	self.X, self.Y, self.Z, self.Go = Xt, Yt, Zt, Got
+	self:SetLastPosition( Vector ( self.X, self.Y, self.Z ))
+	Msg("The X is "..self.X.." the Y is "..self.Y.." and the Z is"..self.Z.."/n")
+	if ( self.Go == 1 ) then
 		self:SetSchedule( SCHED_FORCED_GO_RUN )
-	else
+	elseif ( self.Go == 0 ) or ( Got == nil ) then
 		self:SetSchedule( SCHED_IDLE_STAND )
 	end
 end
+
+
+
+
+
+
