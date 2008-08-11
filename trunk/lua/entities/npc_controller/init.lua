1ebf191ee54a51b5b40c574e4d7101e66f6df53a
Hitman271 <Hitman271@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-08-11 18:30:46 +0000

    Added the working wired npc!
    ---
    yaml
    ---
    svn_rev: 34
    current_ref: refs/heads/master
    current_commit: 55ddfd492d05b2eb8b5921c32de67223e333e6bc
    head_branch: refs/heads/master
    migrated_from: 3

---
+++ trunk/lua/entities/npc_controller/init.lua
@@ -0,0 +1,54 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( 'shared.lua' )
+
+local ison = 0
+
+local model = "models/jaanus/wiretool/wiretool_siren.mdl"
+
+function ENT:Initialize() 
+	self.Entity:SetModel( model ) 	
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )     
+	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )   
+	self.Entity:SetSolid( SOLID_VPHYSICS )               
+	local phys = self.Entity:GetPhysicsObject()  
+		if WireAddon then //Changed this part to conform
+			self.Inputs = Wire_CreateInputs(self.Entity, { "X", "Y", "Z", "Go" })
+			self.Outputs = Wire_CreateOutputs(self.Entity, { "On"})
+		end 
+	if (phys:IsValid()) then  		
+		phys:Wake()  	
+	end 
+ 
+end   
+
+function ENT:TriggerInput( iname, value )
+	if ( NPCGPS == nil ) then 
+		NPCGPS = {}
+		elseif ( iname == "X" ) then
+			NPCGPS[1] = value
+		elseif ( iname == "Y" ) then
+			NPCGPS[2] = value
+		elseif ( iname == "Z" ) then
+			NPCGPS[3] = value
+		elseif ( iname == "Go" ) then
+			NPCGPS[4] = value 
+			self.ison = value
+	end
+end
+
+function ENT:Think()
+	if ( self.ison == 1 ) then
+		self:SetOverlayText( " Npc is moving " )
+	else
+		self:SetOverlayText( " Npc is idle " )
+	end
+	if WireAddon then
+		Wire_TriggerOutput( self.Entity, "On", self.ison )
+	end
+end
+	
+
+
+
+
