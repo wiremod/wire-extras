b23280de36efb95e9612caea41e82a5af9e21d30
Hitman271 <Hitman271@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-09-02 03:12:22 +0000

    --- yaml --- svn_rev: 46 current_ref: refs/heads/master current_commit: 07e119ff988c74ac32cc5fbaf37714497cddfd47 head_branch: refs/heads/master migrated_from: v3

---
+++ trunk/lua/entities/damage_scaler/shared.lua
@@ -0,0 +1,15 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_gmodentity"
+
+
+ENT.PrintName			= ""
+ENT.Author			= ""
+
+ENT.Contact			= ""
+ENT.Purpose			= ""
+ENT.Instructions		= ""
+ENT.Category			= ""
+
+ENT.AdminSpawnable		= "false"
+ENT.Spawnable			= "false"
+
