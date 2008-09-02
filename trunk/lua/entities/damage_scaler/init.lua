d55963fcf361891c0ad82e713ebcef97fee5601e
Hitman271 <Hitman271@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-09-02 03:12:22 +0000

    Added the Wire Damage Scale
    ---
    yaml
    ---
    svn_rev: 46
    current_ref: refs/heads/master
    current_commit: 07e119ff988c74ac32cc5fbaf37714497cddfd47
    head_branch: refs/heads/master
    migrated_from: v3

---
+++ trunk/lua/entities/damage_scaler/init.lua
@@ -0,0 +1,61 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( 'shared.lua' )
+
+local dam = 0
+local takedam = 0
+local ison = 0
+local ref = 0.2
+
+ENT.WireDebugName = "Damage Scale"
+
+function ENT:Initialize() 
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )     
+	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )   
+	self.Entity:SetSolid( SOLID_VPHYSICS )               
+	local phys = self.Entity:GetPhysicsObject()  
+	    	if  WireAddon then 
+			self.Inputs = Wire_CreateInputs(self.Entity, { "On", "Refresh" })	
+	    		self.Outputs = Wire_CreateOutputs(self.Entity, { "Damage"})
+		end
+	if (phys:IsValid()) then  		
+		phys:Wake()  	
+	end 
+end
+
+function ENT:TriggerInput(iname, value)
+	if (iname == "On") then
+		ison = value
+	elseif (iname == "Refresh") then
+		ref = value
+	end
+end
+
+function ENT:OnTakeDamage(dmginfo)
+	if (ison == 1) then
+		dam = dmginfo:GetDamage()
+	else
+		dam = 0
+	end
+end
+
+function ENT:Think()
+	if ( ison == 1 ) then
+		self:SetOverlayText( "The damage is "..dam )
+			if ( dam > 0 ) then
+				Wire_TriggerOutput(self.Entity, "Damage", dam)
+				dam = 0 		
+					if ( ref >= 0 )	then	 //It's not going back in time btw
+						self.Entity:NextThink( CurTime() + ref )
+						return true
+					end
+			else
+				Wire_TriggerOutput(self.Entity, "Damage", 0)
+				self.Entity:NextThink( CurTime() + 0 )
+			end 
+	else
+		self:SetOverlayText( " We are off and ergo nothing is happnin " )
+	end
+end
+
+
