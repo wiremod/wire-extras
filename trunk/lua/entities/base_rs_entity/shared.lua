6db092c04856e2fb487d3f3bdd351ace74bc4745
freefall <freefall@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2008-11-21 14:38:54 +0000

    --- yaml --- svn_rev: 66 current_ref: refs/heads/master current_commit: 3f2812fab50592e240b1b75d72c9f2237ea7fee0 head_branch: refs/heads/master migrated_from: v3

--- trunk/lua/entities/base_rs_entity/shared.lua
+++ trunk/lua/entities/base_rs_entity/shared.lua
@@ -3,3 +3,6 @@ ENT.Base = "base_wire_entity"
 ENT.PrintName = "Base Radiosystems Antenna"
 ENT.Author = "philxyz"
 ENT.Contact = "wiremod.com"
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
