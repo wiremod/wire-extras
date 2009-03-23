3fa491ad451cd4cd05a2040bed6ae7f872df7a3c
Hitman271 <Hitman271@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2009-03-23 02:03:19 +0000

    --- yaml --- r: 82 b: refs/heads/master c: 5168834ba3c760f7f3f1dd2268011d057088ef2c h: refs/heads/master v: v3

--- trunk/lua/entities/damage_scaler/shared.lua
+++ trunk/lua/entities/damage_scaler/shared.lua
@@ -1,9 +1,9 @@
 ENT.Type 			= "anim"
-ENT.Base 			= "base_gmodentity"
+ENT.Base 			= "base_wire_entity"
 
 
-ENT.PrintName			= ""
-ENT.Author			= ""
+ENT.PrintName			= "Damage Scale"
+ENT.Author			= "Hitman"
 
 ENT.Contact			= ""
 ENT.Purpose			= ""
