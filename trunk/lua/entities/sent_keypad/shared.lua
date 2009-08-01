8bd3bd5deb46e1dbf8cae2eba125bd900c90119e
wireteam <wireteam@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2009-08-01 10:49:00 +0000

    --- yaml --- r: 93 b: refs/heads/master c: 6e0a0b36667e8264018b1b2d58a0ee4708b4f5ad h: refs/heads/master i:   91: dff8bab9ee04da893ba56462dcfc9ed8adb3532c v: v3

--- trunk/lua/entities/sent_keypad/shared.lua
+++ trunk/lua/entities/sent_keypad/shared.lua
@@ -1,9 +1,13 @@
 ENT.Type			= "anim"
-ENT.Base			= "base_gmodentity"
+ENT.Base			= "base_wire_entity"
+
+if not WireVersion then
+	timer.Simple(0, function(ENT) if not WireVersion then ENT.Base = "base_gmodentity" end end, ENT)
+end
 
 ENT.PrintName		= "Keypad"
-ENT.Author			= "Robbis_1 (Killer HAHA)"
-ENT.Contact			= "Robbis_1 at Facepunch Studios"
+ENT.Author			= "Robbis_1 (Killer HAHA) and TomyLobo"
+ENT.Contact			= "Robbis_1 and TomyLobo at Facepunch Studios"
 ENT.Purpose			= ""
 ENT.Instructions	= ""
 
