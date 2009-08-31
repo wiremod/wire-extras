487201935b7f16e23e70a94aedd53f7715e17b22
wireteam <wireteam@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2009-08-31 00:41:17 +0000

    --- yaml --- svn_rev: 109 current_ref: refs/heads/master current_commit: 3eb6e8ea5aeb6f4ff3c2c3d05a6543cbb5599981 head_branch: refs/heads/master index:   107: 75b4c988160ea264cf2ffde8be461f09302a568c migrated_from: v3

---
+++ trunk/lua/entities/gmod_wire_string/shared.lua
@@ -0,0 +1,13 @@
+
+
+ENT.Type 			= "anim"
+ENT.Base 			= "base_wire_entity"
+
+ENT.PrintName		= ""
+ENT.Author			= ""
+ENT.Contact			= ""
+ENT.Purpose			= ""
+ENT.Instructions	= ""
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
