2c9c7e11d9460aef41311412dcc986db04e80367
TomyLobo <TomyLobo@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2009-12-12 09:15:50 +0000

    [Touchplate] Added AddCSLuaFile, merged shared.lua into cl_init.lua, added a tool and a duplicator hook.
    ---
    yaml
    ---
    svn_rev: 137
    current_ref: refs/heads/master
    current_commit: 547f2933851f69107879637fce659e2a14f707b5
    head_branch: refs/heads/master
    migrated_from: v3

---
+++ trunk/lua/weapons/gmod_tool/stools/wire_touchplate.lua
@@ -0,0 +1,29 @@
+WireToolSetup.open( "touchplate", "Touchplate", "gmod_wire_touchplate", nil, "Touchplates" )
+
+if CLIENT then
+	language.Add( "Tool_wire_touchplate_name", "Wired Touch Plate" )
+	language.Add( "Tool_wire_touchplate_desc", "Spawns a touch plate for use with the wire system." )
+	language.Add( "Tool_wire_touchplate_0", "Primary: Create/Update touch plate, Secondary: Copy settings" )
+end
+WireToolSetup.BaseLang()
+
+--WireToolSetup.SetupMax( 20, "wire_indicators", "You've hit indicators limit!" )
+
+if SERVER then
+	function TOOL:GetConVars()
+		return self:GetClientNumber( "only_players" ) ~= 0
+	end
+
+	function TOOL:MakeEnt( ply, model, Ang, trace )
+		return MakeWireTouchplate( ply, trace.HitPos, Ang, model, self:GetConVars() )
+	end
+end
+
+TOOL.ClientConVar = {
+	model = "models/props_phx/construct/metal_plate1.mdl",
+	only_players = "1",
+}
+
+function TOOL.BuildCPanel(panel)
+	panel:CheckBox("Only trigger for players", "wire_touchplate_only_players")
+end
