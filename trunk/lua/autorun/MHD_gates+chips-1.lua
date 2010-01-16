7bc890e2b0db136f1f4d3842f5abd496056e4034
TomyLobo <TomyLobo@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-01-16 05:30:10 +0000

    --- yaml --- svn_rev: 176 current_ref: refs/heads/master current_commit: db89f796be0da647070ca39957afa6b1ec24722a head_branch: refs/heads/master migrated_from: v3

--- trunk/lua/autorun/MHD_gates+chips-1.lua
+++ trunk/lua/autorun/MHD_gates+chips-1.lua
@@ -19,7 +19,7 @@ local function AddMHDGates()
 	}
 	//Q: What is Phi?
 	//A: Phi is a irrational decimal value of approx. 1.6180339887...
-	//   Phi resembles the ratio of the sides of an ordinay A4 paper sheet. -- that is bullshit, A4 is 1.41:1, very far from phi. The "Letter" format is closer.
+	//   Phi resembles the ratio of the sides of an ordinay A4 paper sheet. -- that is bullshit, A4 is 1.414:1, very far from phi
 	//   Phi resembles the screen ratio of widescreens (16:9 or 16:10 is roughly the same as Phi:1).
 	//   Phi is the approximate ratio between two sucsessing fibbonachi numbers (1,1,2,3,5,8,13,21,35...).
 	//   Phi resembles the expansion coefficient of snail shells.
