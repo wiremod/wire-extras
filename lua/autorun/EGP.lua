d4127d4100cce2258ea0bcf18194571991db9605
goluch <goluch@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-03-04 13:45:54 +0000

    Sorry Guys Found a very silly bug with EGP2 that atm i can not fix reverting to EGP1

--- lua/autorun/EGP.lua
+++
@@ -1,6 +0,0 @@
-if not EGP then
-	include("EGP/EGP_Shared.lua")
-	AddCSLuaFile("EGP/EGP_Shared.lua")
-end
---This should be done by E2 with a implemented code.
---But incase it fucks up then this will make sure the lib is up and running.
