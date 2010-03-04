69e7cc15f0de8811435fa3afa81f6d1c4c37e9eb
goluch <goluch@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-03-04 04:39:04 +0000

    --- yaml --- svn_rev: 185 current_ref: refs/heads/master current_commit: e28e3ac59619ef967a7126cf1a2fa1a5d7cb3cc6 head_branch: refs/heads/master migrated_from: v3

---
+++ trunk/lua/EGP/USM/Default.lua
@@ -0,0 +1,108 @@
+EGP.USM.Default = {
+		{ "Short", "X" },
+		{ "Short", "Y" },
+		{ "Short", "W" },
+		{ "Short", "H" },
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" },
+}
+
+EGP.USM.E = {}
+
+EGP.USM.box = {
+		{ "Short", "X" },
+		{ "Short", "Y" },
+		{ "Short", "W" },
+		{ "Short", "H" },
+		{ "Short", "Ang" },
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" },
+		{ "String", "material" },
+}
+
+EGP.USM.boxoutline = {
+		{ "Short", "X" },
+		{ "Short", "Y" },
+		{ "Short", "W" },
+		{ "Short", "H" },
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" },
+		{ "String", "material" },
+}
+
+EGP.USM.text = {
+		{ "Short", "X" },
+		{ "Short", "Y" },
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" },
+		{ "String", "text" },
+		{ "Char", "falign" },
+		{ "Short", "fsize" },
+		{ "Char", "fid" }
+}
+
+EGP.USM.textl = {
+		{ "Short", "X" },
+		{ "Short", "Y" },
+		{ "Short", "W" },
+		{ "Short", "H" },
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" },
+		{ "String", "text" },
+		{ "Char", "falign" },
+		{ "Short", "fsize" },
+		{ "Char", "fid" }
+}
+
+EGP.USM.line = {
+		{ "Short", "X" },
+		{ "Short", "Y" },
+		{ "Short", "X1" },
+		{ "Short", "Y1" },
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" },
+}
+
+EGP.USM.circle = {
+		{ "Short", "X" },
+		{ "Short", "Y" },
+		{ "Short", "W" },
+		{ "Short", "H" },
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" }
+}
+
+EGP.USM.triangle = {
+		{ "Short", "X1" },
+		{ "Short", "Y1" },
+		{ "Short", "X2" },
+		{ "Short", "Y2" },
+		{ "Short", "X3" },
+		{ "Short", "Y3" },
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" }
+}
+EGP.USM.poly = {
+		{ "Byte", "R" },
+		{ "Byte", "G" },
+		{ "Byte", "B" },
+		{ "Byte", "A" },
+		{ "String", "material" },
+		{ "VertexList", "vertices" }
+}
