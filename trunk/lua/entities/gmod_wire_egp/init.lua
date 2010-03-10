7d5b1e479807f13cfadd8e3600763b388576303f
goluch <goluch@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-03-10 00:20:36 +0000

    Added EGP1 disabler. If EGP2 is detected EGP1 will not be enabled. also added cvar "egp_enabled" default 1. When cvar is 0 egp is disabled. CVAR CAN ONLY BE USED IN SERVER CGF USE IT IN CONSOLE IF YOUR STUPID!
    ---
    yaml
    ---
    svn_rev: 188
    current_ref: refs/heads/master
    current_commit: 2a4a5caae6f3208db86d206f79d0eafffcda7437
    head_branch: refs/heads/master
    migrated_from: v3

--- trunk/lua/entities/gmod_wire_egp/init.lua
+++ trunk/lua/entities/gmod_wire_egp/init.lua
@@ -1,3 +1,9 @@
+--EGP2 Compatability
+if EGP then return end -- should detect an egp2 install
+if not egp_enabled then egp_enabled = CreateConVar("egp_enabled", "1", FCVAR_ARCHIVE) end
+if egp_enabled:GetInt() == 0 then return end
+--------------------------------
+
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include('shared.lua')
