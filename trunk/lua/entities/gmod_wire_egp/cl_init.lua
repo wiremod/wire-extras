a8f4cbf2d27d21449ca2db28419be71149896ebd
goluch <goluch@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-06-04 03:05:36 +0000

    --- yaml --- svn_rev: 207 current_ref: refs/heads/master current_commit: 1627fcb0f64ae8de9f9f9dbd3b0edcad06d37c56 head_branch: refs/heads/master migrated_from: v3

--- trunk/lua/entities/gmod_wire_egp/cl_init.lua
+++ trunk/lua/entities/gmod_wire_egp/cl_init.lua
@@ -1,5 +1,3 @@
---ToDo: add angle and texture
---Note: anybody that knows how to do this pleae do.
 include('shared.lua')
 
 local function validEGP(ent)
@@ -12,24 +10,7 @@ function ENT:Initialize()
 	self:InitializeShared()
 	
 	self.GPU = WireGPU(self.Entity)
-	self.Render = {
-	--EGP HOME SCREEN!
-	--By mattwd0526
-		--Dimond
-		{image="box",X=0,Y=0,W=512,H=512,material="",Ang=0,R=10, G=10, B=10, A=255},
-		{image="box",X=256,Y=256,W=362,H=362,material="",Ang=135,R=75, G=75, B=200, A=255},
-		{image="box",X=256,Y=256,W=340,H=340,material="",Ang=135,R=10, G=10, B=10, A=255},
-		{image="text",X=229,Y=28,text="E",falign=0,fsize=100,fid=4,R=200, G=50, B=50, A=255},
-		{image="text",X=50,Y=200,text="G",falign=0,fsize=100,fid=4,R=200, G=50, B=50, A=255},
-		{image="text",X=400,Y=200,text="P",falign=0,fsize=100,fid=4,R=200, G=50, B=50, A=255},
-		{image="text",X=228,Y=375,text="2",falign=0,fsize=100,fid=4,R=200, G=50, B=50, A=255},
-		{image="box",X=256,Y=256,W=256,H=256,material="expression 2/cog",Ang=45,R=255, G=50, B=50, A=255},
-		{image="box",X=128,Y=241,W=256,H=30,material="",Ang=0,R=10, G=10, B=10, A=255},
-		{image="box",X=241,Y=128,W=30,H=256,material="",Ang=0,R=10, G=10, B=10, A=255},
-		{image="circle",X=256,Y=256,W=70,H=70,material="",R=255,G=50,B=50,A=255},
-		{image="box",X=256,Y=256,W=362,H=362,material="gui/center_gradient",Ang=135,R=75, G=75, B=200, A=75},
-		{image="box",X=256,Y=256,W=362,H=362,material="gui/center_gradient",Ang=45,R=75, G=75, B=200, A=75}
-	}
+	self.Render = EGP.HomeScreen
 	
 	self.FirstDraw = true
 	self.NeedsRender = true
@@ -52,15 +33,8 @@ function ENT:Draw()
 	self.Entity.DrawEntityOutline = function() end
 	self.Entity:DrawModel()
 	if self.NeedsRender then
-		self.GPU:RenderToGPU(function()
-			local RatioX = 1
-			local w = 1024
-			local h = 1024
-			--add changable backround colour some time.
-			--render.SetViewPort(0, 0, w, h) --test
-			surface.SetDrawColor(0, 0, 0, 255)
-			surface.DrawRect(0, 0, w, h)
-			--render.Clear( 0, 0, 0, 0, true )
+		self.GPU:RenderToGPU(function()	
+			render.Clear( 0, 0, 0, 0 )
 			EGP.Process(self)
 		end)
 		self.NeedsRender = false
@@ -69,3 +43,4 @@ function ENT:Draw()
 	self.GPU:Render()
 	Wire_Render(self.Entity)
 end
+
