aa44c74292b3306b0e38c0567b96ee99b7418442
goluch <goluch@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-06-05 01:19:52 +0000

    EGP Update
    ---
    yaml
    ---
    svn_rev: 209
    current_ref: refs/heads/master
    current_commit: 3bb014c34f7187220cdd6a3c1d698bb419b6c391
    head_branch: refs/heads/master
    migrated_from: 3

--- trunk/lua/entities/gmod_wire_egp/lib/log.lua
+++ trunk/lua/entities/gmod_wire_egp/lib/log.lua
@@ -2,11 +2,13 @@
 --Plz make sure you keep this file up to date.
 
 --SVN REVISHION NUMBER
-EGP.Rev = "2.9"
+EGP.Rev = "3.1"
 ----------------------
 
 --Patch Notes!
 --Date,Rev,Note!
+EGP.AddLog("5/6/2010","3.1","Added Entity: support to all egp functions..")
+EGP.AddLog("5/6/2010","3.1","Added e2 Prop Protection to all functions.")
 EGP.AddLog("4/6/2010","2.9","In game tests complete.")
 EGP.AddLog("4/6/2010","2.9","e2 helper functions added.")
 EGP.AddLog("4/6/2010","2.9","New e2 functions added.")
