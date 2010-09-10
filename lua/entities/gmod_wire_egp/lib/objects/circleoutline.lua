5d216b1c5d184892fc405cb2eed95a0596f1413d
Divran <Divran@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-09-10 15:34:09 +0000

    [EGP] - Outline objects now have a line width argument. - Parenting (should) works 100% More info: http://www.wiremod.com/forum/developers-showcase/20727-egp-v3-total-remake-12.html#post211476

--- lua/entities/gmod_wire_egp/lib/objects/circleoutline.lua
+++ lua/entities/gmod_wire_egp/lib/objects/circleoutline.lua
@@ -1,6 +1,7 @@
 local Obj = EGP:NewObject( "CircleOutline" )
 Obj.material = ""
 Obj.angle = 0
+Obj.size = 1
 Obj.Draw = function( self )
 	if (self.a>0 and self.w > 0 and self.h > 0) then
 		local vertices = {}
@@ -23,26 +24,28 @@ Obj.Draw = function( self )
 			if (k+1<=#vertices) then
 				local x, y = v.x, v.y
 				local x2, y2 = vertices[k+1].x, vertices[k+1].y
-				surface.DrawLine( x, y, x2, y2 )
+				EGP:DrawLine( x, y, x2, y2, self.size )
 			end
 		end
 		
-		surface.DrawLine( vertices[#vertices].x, vertices[#vertices].y, vertices[1].x, vertices[1].y )
+		EGP:DrawLine( vertices[#vertices].x, vertices[#vertices].y, vertices[1].x, vertices[1].y, self.size )
 	end
 end
 Obj.Transmit = function( self )
 	EGP.umsg.Short( self.angle )
+	EGP.umsg.Short( self.size )
 	self.BaseClass.Transmit( self )
 end
 Obj.Receive = function( self, um )
 	local tbl = {}
 	tbl.angle = um:ReadShort()
+	tbl.size = um:ReadShort()
 	table.Merge( tbl, self.BaseClass.Receive( self, um ) )
 	return tbl
 end
 Obj.DataStreamInfo = function( self )
 	local tbl = {}
 	table.Merge( tbl, self.BaseClass.DataStreamInfo( self ) )
-	table.Merge( tbl, { material = self.material, angle = self.angle } )
+	table.Merge( tbl, { material = self.material, angle = self.angle, size = self.size } )
 	return tbl
 end
