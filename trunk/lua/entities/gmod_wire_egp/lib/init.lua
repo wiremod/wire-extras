9e6eb2a92d903bcaaa703e4e0c9d129a2dc7c375
Divran <Divran@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-09-15 16:36:53 +0000

    - Fixed egpCanSendUmsg() - Made WLK:egpCursor(E) return -1,-1 when aiming off the screen instead of values greater than 512 or smaller than 0 (only for regular screens, not for emitters) - Added function WLK:egpParentToCursor(N) (You can also parent to the cursor by doing WLK:egpParent(N,-1), the ParentToCursor function is just for people who don't know it's -1). Note that this "parenting" only makes the object move to YOUR cursor. As in other people will see the object attached to THEIR cursor. Also note that this forces the screen to re-calculate the positions of all objects on the screen, so it would be best if you unparented the object from your cursor every time you look away from the screen (when egpCursor returns -1,-1) - Fixed this* error. *: Look at the post
    Post:
    http://www.wiremod.com/forum/developers-showcase/20727-egp-v3-total-remake-13.html#post212242
    
    
    ---
    yaml
    ---
    svn_rev: 237
    current_ref: refs/heads/master
    current_commit: 99e88e2d5029f1841604a8637bda251bdcf6887f
    head_branch: refs/heads/master
    migrated_from: v3

--- trunk/lua/entities/gmod_wire_egp/lib/init.lua
+++ trunk/lua/entities/gmod_wire_egp/lib/init.lua
@@ -27,7 +27,3 @@ EGP.ConVars.MaxPerSec = CreateConVar( "wire_egp_max_umsg_per_sec", 10, { FCVAR_N
 EGP.ConVars.AllowEmitter = CreateConVar( "wire_egp_allow_emitter", 1, { FCVAR_NOTIFY, FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE  }  )
 EGP.ConVars.AllowHUD = CreateConVar( "wire_egp_allow_hud", 1, { FCVAR_NOTIFY, FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE  }  )
 EGP.ConVars.AllowScreen = CreateConVar( "wire_egp_allow_screen", 1, { FCVAR_NOTIFY, FCVAR_SERVER_CAN_EXECUTE, FCVAR_ARCHIVE  }  )
-
--- If other addons want to do something with EGP, this makes sure everything has loaded first.
--- It needs to be inside an Initialize hook because if it isn't, the other addon's hook.Add may not have been run yet.
-hook.Add("Initialize","EGP_Initialize",function() hook.Call("WireEGP_Initialize", {}) end)
