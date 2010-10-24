9c36c741f8cd5d64a627aa8d7b42c7680bb2a53e
Divran <Divran@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-10-24 21:11:26 +0000

    [EGP] I broke some stuff in the previous update. Fixed those, as well as some other minor things
    ---
    yaml
    ---
    svn_rev: 253
    current_ref: refs/heads/master
    current_commit: 4e35078cb1a7c536da5c4a1bbefd83954a6d7e78
    head_branch: refs/heads/master
    migrated_from: v3

--- trunk/lua/entities/gmod_wire_egp/lib/EGPLib/FrameControl.lua
+++ trunk/lua/entities/gmod_wire_egp/lib/EGPLib/FrameControl.lua
@@ -8,17 +8,19 @@ EGP.Frames = {}
 
 function EGP:SaveFrame( ply, Ent, index )
 	if (!EGP.Frames[ply]) then EGP.Frames[ply] = {} end
-	EGP.Frames[ply][index] = Ent.RenderTable
+	EGP.Frames[ply][index] = table.Copy(Ent.RenderTable)
 end
 
 function EGP:LoadFrame( ply, Ent, index )
 	if (!EGP.Frames[ply]) then EGP.Frames[ply] = {} return false end
 	if (SERVER) then
-		return (EGP.Frames[ply][index] != nil), EGP.Frames[ply][index]
+		local bool = (EGP.Frames[ply][index] != nil)
+		if (!bool) then return false end
+		return true, table.Copy(EGP.Frames[ply][index])
 	else
 		local frame = EGP.Frames[ply][index]
 		if (!frame) then return false end
-		Ent.RenderTable = frame
+		Ent.RenderTable = table.Copy(frame)
 		Ent:EGP_Update()
 	end
 end
