ca2cb8ba2d58c45b21265c87436f40ad1d5554f0
Divran <Divran@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2010-11-28 02:58:17 +0000

    Cleaning up. Removed several crappy entities that have no use, and fixed the two servos.
    ---
    yaml
    ---
    svn_rev: 260
    current_ref: refs/heads/master
    current_commit: cb99419a154b3ece111130c08ab9422926c4bfd2
    head_branch: refs/heads/master
    migrated_from: v3

--- trunk/lua/entities/gmod_wire_notifier/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type 		= "anim"
-ENT.Base 		= "base_wire_entity"
-
-ENT.PrintName	= "Notifier (Wire)"
-ENT.Author		= "High6"
-ENT.Contact		= ""
-
-ENT.Spawnable			= false
-ENT.AdminSpawnable		= false
