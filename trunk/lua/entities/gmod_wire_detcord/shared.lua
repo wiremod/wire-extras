2aac685ab0ce244068b5681fde9d2e78549ade6e
anticept <anticept@fc6888ea-bd4e-0410-a5f2-80b195ffdc8f>
2012-03-15 06:30:14 +0000

    --- yaml --- svn_rev: 284 current_ref: refs/heads/master current_commit: bd9bfcaa105d6eec166cdae59dd5c5bbf565e8b1 head_branch: refs/heads/master migrated_from: v3

--- trunk/lua/entities/gmod_wire_detcord/shared.lua
+++
@@ -1,8 +0,0 @@
-ENT.Type 		= "anim"
-ENT.Base 		= "base_wire_entity"
-ENT.PrintName		= "Detonation Cord"
-ENT.Author		= "philxyz"
-ENT.Contact		= "wiremod.com"
-
-ENT.Spawnable			= false
-ENT.AdminSpawnable		= false
