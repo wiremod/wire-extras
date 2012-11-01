bf557721fb35760f3ed28082a176f8044a60f469
CoreyLee Hassell <anticept@gmail.com>
2012-10-31 23:11:50 -0400

    Removed the non-broken stuff (oops!)
    ---
    yaml
    ---
    svn_rev: 298
    current_ref: refs/heads/broken
    current_commit: 81ab6a6e6a8e22912a11fd8e72d9e21ea5b3436b
    head_branch: refs/heads/master
    migrated_from: 3

--- branches/broken/lua/entities/gmod_wire_buoyancy/shared.lua
+++
@@ -1,9 +0,0 @@
-
-ENT.Type 			= "anim"
-ENT.Base 			= "base_wire_entity"
-
-ENT.PrintName			= "Buoyancy Controller"
-ENT.Author				= "RabidToaster"
-
-ENT.Spawnable			= false
-ENT.AdminSpawnable		= false
--- branches/broken/lua/entities/wired_door/cl_init.lua
+++
@@ -1,7 +0,0 @@
-
-ENT.Spawnable			= false
-ENT.AdminSpawnable		= false
-
-include('shared.lua')
-
-language.Add( "wired_door", "Door with Wire" )
