14ae3c2c52a98795370365f2de44a27090ac472d
Phil Costin <philcostin@hotmail.com>
2014-07-13 01:43:15 +0100

    Re-add a fixed Wire Directional Radio Kit
    ---
    yaml
    ---
    svn_rev: 336
    current_ref: refs/heads/master
    current_commit: 43458a0c536e53229c0b00f42833c708f99bf883
    head_branch: refs/heads/master
    migrated_from: 3

---
+++ trunk/lua/entities/base_wdr_entity/shared.lua
@@ -0,0 +1,5 @@
+ENT.Type = "anim"
+ENT.Base = "base_wire_entity"
+ENT.PrintName = "Base Wire Directional Radio Entity"
+ENT.Author = "philxyz"
+ENT.Contact = ""
--- trunk/lua/entities/ra_log/init.lua
+++ trunk/lua/entities/ra_log/init.lua
@@ -4,7 +4,6 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function ENT:Initialize()
-	self.BaseClass:Initialize()
 	self.gain = 9.6 -- dB
 	self.pol = 2 -- Horizontal Polarization
 	self.beamWidth = 20.0 -- Degrees
@@ -12,9 +11,10 @@ function ENT:Initialize()
 	self.txchannels = {} -- tx data
 	self.txwatts = 0 -- tx power
 	self:SetModel("models/radio/ra_log.mdl")
-	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_VPHYSICS)
 	self:SetSolid(SOLID_VPHYSICS)
+	self:PhysicsInit(SOLID_VPHYSICS)
+
 	local phys = self:GetPhysicsObject()
-	if(phys:IsValid()) then phys:Wake() end
+	if phys:IsValid() then phys:Wake() end
 end
