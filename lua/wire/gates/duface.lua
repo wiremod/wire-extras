cfdd3f930d5117b6519f55b42195c9fc3f70f33c
Garbius <garbius@hotmail.com>
2015-07-29 23:01:18 +0200

    Fixed NULL error in entity_ident gate.

--- lua/wire/gates/duface.lua
+++ lua/wire/gates/duface.lua
@@ -117,8 +117,7 @@ GateActions["entity_ident"] = {
 	end,
 	label = function (Out, A)
 		local strEnt = "(none)"
-		if (IsEntity (Out)) then strEnt = Out:GetName () end
-		return string.format ("%s = %s", A, strEnt)
+		return string.format ("%s = %s", A, tostring(Out))
 	end
 }
 // Parent
