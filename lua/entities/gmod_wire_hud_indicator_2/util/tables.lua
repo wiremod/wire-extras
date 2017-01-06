dda328de108e49a69cc85e6afeea093d42f0e3fe
Divran <arviddivran@gmail.com>
2017-01-06 05:00:16 +0100

    Erased overwrite of PrintTable

--- lua/entities/gmod_wire_hud_indicator_2/util/tables.lua
+++ lua/entities/gmod_wire_hud_indicator_2/util/tables.lua
@@ -3,28 +3,6 @@ function PrintTitledTable( title, t )
 	PrintTable( t, 8 )
 end
 
-function PrintTable (t, indent, done)
-  if( type(t) == "table" ) then
-    done = done or {}
-    indent = indent or 0
-    for key, value in pairs (t) do
-      indentStr = string.rep (" ", indent) -- indent it
-      if type (value) == "table" and not done [value] then
-        done [value] = true
-        print( indentStr..tostring(key).."{")
-        PrintTable (value, indent+3, done)
-      else
-        print(indentStr..tostring (key), " = ", tostring(value))
-      end
-    end
-
-    indentStr = string.rep (" ", indent-3) -- indent it
-    print( indentStr.."}" )
-  else
-    print( "Not a table!" )
-  end
-end
-
 function CatTables( tables )
 	local result = {}
 	
