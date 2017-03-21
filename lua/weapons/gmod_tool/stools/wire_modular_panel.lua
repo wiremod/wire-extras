4c5a144dede11efeb1e5e7defbdde5eb300b954a
UAVXP <uavxp29@gmail.com>
2017-03-21 17:33:21 +0200

    Replaced utilx with util library

--- lua/weapons/gmod_tool/stools/wire_modular_panel.lua
+++ lua/weapons/gmod_tool/stools/wire_modular_panel.lua
@@ -150,7 +150,7 @@ function TOOL:UpdateGhostWireTextScreen( ent, player )
 
 	if ( !ent:IsValid() ) then return end
 
-	local tr 	= utilx.GetPlayerTrace( player, player:GetCursorAimVector() )
+	local tr 	= util.GetPlayerTrace( player, player:GetCursorAimVector() )
 	local trace 	= util.TraceLine( tr )
 
 	if (!trace.Hit) then return end
