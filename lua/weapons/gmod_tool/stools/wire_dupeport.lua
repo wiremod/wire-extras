a654f4df684118b8c4c3e9dedb489e85e4406e8b
Deyan Dobromirov <dvdvideo123@gmail.com>
2019-08-06 10:29:25 +0300

    Added: Micro-optimization using `GetEyeTrace`

--- lua/weapons/gmod_tool/stools/wire_dupeport.lua
+++ lua/weapons/gmod_tool/stools/wire_dupeport.lua
@@ -99,8 +99,7 @@ function TOOL:UpdateGhostWireDupePort( ent, player )
 	if ( !ent ) then return end
 	if ( !ent:IsValid() ) then return end
 
-	local tr 	= util.GetPlayerTrace( player, player:GetAimVector() )
-	local trace 	= util.TraceLine( tr )
+	local trace = player:GetEyeTrace()
 	if (!trace.Hit) then return end
 
 	if (trace.Entity && trace.Entity:GetClass() == "gmod_wire_dupeport" || trace.Entity:IsPlayer()) then
