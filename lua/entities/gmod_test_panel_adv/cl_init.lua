4840e76c0a48455622d259a25ed96c47a9dfbb47
CoreyLee Hassell <anticept@gmail.com>
2020-02-11 22:08:34 -0500

    removed anchient and conflicting entity

--- lua/entities/gmod_test_panel_adv/cl_init.lua
+++
@@ -1,8 +0,0 @@
---	gui Panel Test entity - greenarrow
-
-include('shared.lua')
-
-function ENT:Initialize()
-	guiP_PanelInit(self.Entity, 200, 200)		-- Initialize panel
-	self:BuildPanel()							-- calls function in shared.lua (in this in test entity, not the library)
-end
