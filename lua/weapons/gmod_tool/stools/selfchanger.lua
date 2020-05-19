906eca81cba42b160c1b45bc4d3c157438c1cd80
Deyan Dobromirov <dvd_video@abv.bg>
2020-05-19 07:16:43 +0300

    Updated: Self Changer for Gmod 13 by utilizing library `net` (#92)
    * Updated: Self Changer for Gmod 13 by utilizing the `net` library. Fixed: #73
    
    * Removed: local panel variable `pItem`
    
    * Updated: Tooltips as separate translated strings
    
    * Utilized: Notify API `WireLib.AddNotify`

--- lua/weapons/gmod_tool/stools/selfchanger.lua
+++ lua/weapons/gmod_tool/stools/selfchanger.lua
@@ -1,64 +1,84 @@
+TOOL.Category   = "GUI Panels"
+TOOL.Name       = "Self changer"
+TOOL.Command    = nil
+TOOL.ConfigName = ""
 
-TOOL.Category		= "GUI Panels"
-TOOL.Name			= "Self changer"
-TOOL.Command		= nil
-TOOL.ConfigName		= ""
+TOOL.ClientConVar =
+{
+	["widvariable"] = "",
+	["widvalue"]    = "",
+	["widvaluenum"] = "0"
+}
 
 if ( CLIENT ) then
-    language.Add( "Tool_selfchanger_name", "Developer Tool" )
-    language.Add( "Tool_selfchanger_desc", "changes self....." )
-    language.Add( "Tool_selfchanger_0", "Primary: set self value, Sec: reload draw params" )
-	--language.Add("Tool_selfchanger_widname", "Widget name:")
-	
-	language.Add("Tool_selfchanger_widvariable", "Variable:")
-	language.Add("Tool_selfchanger_widvalue", "Value:")
-	--language.Add("Tool_selfchanger_themename", "Colour scheme:")
-	
-	function umGetNewSet(um)
-		
-		local ent = um:ReadEntity()
-		local var = um:ReadString()
-		local val = um:ReadFloat()
-		Msg("setting "..var.."\n")
-		ent[var] = val
+	language.Add( "tool.selfchanger.name", "Developer Tool" )
+	language.Add( "tool.selfchanger.desc", "Changes self....." )
+	language.Add( "tool.selfchanger.0", "Primary: Set self value, Secondary: reload draw parameters" )
+	language.Add( "tool.selfchanger.widvariable", "Variable:")
+	language.Add( "tool.selfchanger.widvariable_con", "Write variable entity hash name here")
+	language.Add( "tool.selfchanger.widvalue", "Value:")
+	language.Add( "tool.selfchanger.widvalue_con", "Write desired variable value here")
+	language.Add( "tool.selfchanger.widvaluenum", "Force value number conversion" )
+	language.Add( "tool.selfchanger.widvaluenum_con", "Check this for force converting the value to a number" )
+
+	function setEntitySetting()
+		local ent = net.ReadEntity()
+		local var = net.ReadString()
+		local val = net.ReadString()
+		local bnv = net.ReadBool()
+
+		if(bnv) then
+			ent[var] = (tonumber(val) or 0)
+		else
+			ent[var] = (tostring(val) or "")
+		end
+
+		local set = tostring(ent and ent[var] or "N/A")
+		local msg = "["..tostring(var).."] = <"..tostring(val)..">{"..set.."}"
+
+		WireLib.AddNotify(LocalPlayer(), "Client: "..msg, NOTIFY_GENERIC, 6)
 	end
-	usermessage.Hook("umsgSelfNewSet", umGetNewSet) 
 
-
+	net.Receive("selfchangerSetEntitySetting", setEntitySetting)
 end
 
+if( SERVER ) then
+	util.AddNetworkString("selfchangerSetEntitySetting")
+end
 
 function TOOL:sendSetVal(ent, var, val)
-	--Msg(string.format("value at ssi = %f\n", value))
-	ent[var] = val
-	local allPlayers = RecipientFilter()
-	allPlayers:AddAllPlayers()
-	umsg.Start("umsgSelfNewSet", allPlayers)	--do we need to send entity with all user messages (so we know which pannel we are talking about?)
-		umsg.Entity(ent)
-		umsg.String(var)
-		umsg.Float(val)
-	umsg.End() 
+	local bnv = (self:GetClientNumber("widvaluenum") ~= 0)
+
+	if(bnv) then
+		ent[var] = (tonumber(val) or 0)
+	else
+		ent[var] = (tostring(val) or "")
+	end
+
+	local set = tostring(ent and ent[var] or "N/A")
+	local msg = "["..tostring(var).."] = <"..tostring(val)..">{"..set.."}"
+
+	WireLib.AddNotify(self:GetOwner(), "Server: "..msg, NOTIFY_GENERIC, 6)
+
+	net.Start("selfchangerSetEntitySetting")
+		net.WriteEntity(ent)
+		net.WriteString(var)
+		net.WriteString(val)
+		net.WriteBool(bnv)
+	net.Broadcast()
 end
-
-TOOL.ClientConVar["widvariable"] = ""
-TOOL.ClientConVar["widvalue"] = ""
-
 
 function TOOL:LeftClick( trace )
 	if trace.Entity && trace.Entity:IsPlayer() then return false end
 	if (CLIENT) then return true end
-	--if (SERVER) then return true end
-	
+
+	local widValue    = self:GetClientInfo("widvalue")
 	local widVariable = self:GetClientInfo("widvariable")
-	local widValue = self:GetClientInfo("widvalue")
-		
+
 	--if (trace.Entity:IsValid() && trace.Entity.guiPanelVersion && trace.Entity.pl == ply) then
 	--trace.Entity[widVariable] = widValue
 	self:sendSetVal(trace.Entity, widVariable, widValue)
-	Msg("done\n")
-		--return true
-	--end
-	
+
 	return true
 end
 
@@ -66,15 +86,15 @@ function TOOL:RightClick( trace )
 	if trace.Entity && trace.Entity:IsPlayer() then return false end
 	--if (CLIENT) then return true end
 	if (SERVER) then return true end
-	
+
 	--local themeName = self:GetClientInfo("themename")
-		
+
 	--if (trace.Entity:IsValid() && trace.Entity.guiPanelVersion && trace.Entity.pl == ply) then
 		--trace.Entity:SetPanelScheme(themeName)
 		--return true
 	--end
 	gpSetupVals(trace.Entity)
-	
+
 	return true
 end
 
@@ -82,12 +102,10 @@ end
 function TOOL:Think()
 end
 
-function TOOL.BuildCPanel(panel)
-
-	panel:AddControl("Header", { Text = "#Tool_wire_modular_panel_name", Description = "#Tool_wire_modular_panel_desc" })
-	
-	panel:AddControl("TextBox", {Label = "#Tool_selfchanger_widvariable", MaxLength = tostring(50), Command = "selfchanger_widvariable"})
-	panel:AddControl("TextBox", {Label = "#Tool_selfchanger_widvalue", MaxLength = tostring(50), Command = "selfchanger_widvalue"})
-	--panel:AddControl("TextBox", {Label = "#Tool_selfchanger_widproperty", MaxLength = tostring(50), Command = "selfchanger_widproperty"})
-	--panel:AddControl("TextBox", {Label = "#Tool_selfchanger_themename", MaxLength = tostring(50), Command = "selfchanger_themename"})
+function TOOL.BuildCPanel(cPanel)
+	cPanel:SetName( "#tool.selfchanger.name" )
+	cPanel:Help   ( "#tool.selfchanger.desc" )
+	cPanel:TextEntry( "#tool.selfchanger.widvariable", "selfchanger_widvariable" ):SetTooltip( "#tool.selfchanger.widvariable_con" )
+	cPanel:TextEntry( "#tool.selfchanger.widvalue"   , "selfchanger_widvalue"    ):SetTooltip( "#tool.selfchanger.widvalue_con"    )
+	cPanel:CheckBox ( "#tool.selfchanger.widvaluenum", "selfchanger_widvaluenum" ):SetTooltip( "#tool.selfchanger.widvaluenum_con" )
 end
